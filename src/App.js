cf28fb9f4bcf985c39fe972866c84e86723d79b0
A-Mounika29 <abburimounika0@gmail.com>
2025-01-17 17:48:48 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div className='container border border-primary rounded mt-5'
+    
+    >
+      <Todo/>
+
+    </div>
+  );
+}
+
+export default App;
